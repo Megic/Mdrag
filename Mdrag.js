148a7d071f7440fb66a2c9d321f278620498f72f
megic <420636220@qq.com>
2015-07-31 15:53:18 +0800

    改变形状功能完善

--- Mdrag.js
+++ Mdrag.js
@@ -1,6 +1,6 @@
 /**
- * author levi
- * url http://levi.cg.am
+ * author Megic
+ *
  */
 
 (function ($) {
@@ -22,17 +22,16 @@
         return this.each(function () {
             var obj = $(this);//父元素，移动范围
             var offset = obj.offset();//父元素边距
-            //隐藏控制器
+            //点击html隐藏控制器
             var curBox;
             $('html').mousedown(function () {
-                $(settings.item,this).removeClass('mdrag-show');
+                $(settings.item, this).removeClass('mdrag-show');
             });
             //监测鼠标移动
             obj.mousemove(function (e) {
                 if (!!moveObj.move) {
                     var posix = !moveObj.move_target ? {'x': 0, 'y': 0} : moveObj.move_target.posix,
                         callback = moveObj.call_down || function () {
-                                // console.log(e.pageY)
                                 $(moveObj.move_target).css({
                                     'top': e.pageY - posix.y,
                                     'left': e.pageX - posix.x
@@ -58,7 +57,7 @@
 
 
             obj.on('mousedown', settings.item, function (e) {
-                if(curBox)curBox.removeClass('mdrag-show');
+                if (curBox)curBox.removeClass('mdrag-show');
                 curBox = $(this);
                 curBox.addClass('mdrag-show');
                 if (!moveObj.move) {
@@ -74,95 +73,92 @@
                     //大小控制器
                     var controller = $(settings.controller, this);
                     controller.unbind('mousedown').mousedown(function (e) {
-                        var clObj=$(this);
-                      var itemW=item.width();
-                        var itemH=item.height();
+                        var clObj = $(this);
+                        var itemW = item.width();
+                        var itemH = item.height();
                         var posix = {
-                            'top':parseInt(item.css('top')),
-                            'left':parseInt(item.css('left')),
-                            'ox':parseInt(item.css('left'))+offset.left+itemW/2,//元素中心坐标
-                            'oy':parseInt(item.css('top'))+offset.top+itemH/2,
+                            'top': parseInt(item.css('top')),
+                            'left': parseInt(item.css('left')),
+                            'ox': parseInt(item.css('left')) + offset.left + itemW / 2,//元素中心坐标
+                            'oy': parseInt(item.css('top')) + offset.top + itemH / 2,
                             'w': itemW,
                             'h': itemH,
                             'x': e.pageX,//鼠标点击坐标
                             'y': e.pageY
                         };
                         var direction;
-                        posix['type']=0;//变化方向
-                        var cType=clObj.attr('data-type');
-                        switch (cType){
+                        posix['type'] = 0;//变化方向
+                        var cType = clObj.attr('data-type');
+                        switch (cType) {
                             case '1'://宽度变化
-                                direction=e.pageY<posix.oy;
-                                clObj.css('cursor','s-resize');
+                                direction = e.pageY < posix.oy;
+                                clObj.css('cursor', 's-resize');
                                 break;
                             case '2'://宽度变化
-                                direction=e.pageX<posix.ox;
-                                clObj.css('cursor','w-resize');
+                                direction = e.pageX < posix.ox;
+                                clObj.css('cursor', 'w-resize');
                                 break;
                             case '3'://整体变化
-                                if((e.pageY>posix.oy)&&(e.pageX>posix.ox)){
-                                    direction='rb';
-                                    clObj.css('cursor','se-resize');
+                                if ((e.pageY > posix.oy) && (e.pageX > posix.ox)) {
+                                    direction = 'rb';
+                                    clObj.css('cursor', 'se-resize');
                                 }//右下
-                                if((e.pageY<posix.oy)&&(e.pageX>posix.ox)){
-                                    direction='rt';
-                                    clObj.css('cursor','ne-resize');
+                                if ((e.pageY < posix.oy) && (e.pageX > posix.ox)) {
+                                    direction = 'rt';
+                                    clObj.css('cursor', 'ne-resize');
                                 }//右上
-                                if((e.pageY>posix.oy)&&(e.pageX<posix.ox)){
-                                    direction='lb';
-                                    clObj.css('cursor','ne-resize');
+                                if ((e.pageY > posix.oy) && (e.pageX < posix.ox)) {
+                                    direction = 'lb';
+                                    clObj.css('cursor', 'ne-resize');
                                 }//左下
-                                if((e.pageY<posix.oy)&&(e.pageX<posix.ox)){
-                                    direction='lt';
-                                    clObj.css('cursor','se-resize');
+                                if ((e.pageY < posix.oy) && (e.pageX < posix.ox)) {
+                                    direction = 'lt';
+                                    clObj.css('cursor', 'se-resize');
                                 }//左上
                                 break;
 
                         }
                         $.extend(moveObj, {
-                            'move': true,'call_up':function(){clObj.css('cursor','crosshair');}, 'call_down': function (e) {
-                                var option={};
-                                var cX=e.pageX - posix.x;//x轴变化
-                                var cY=e.pageY - posix.y;//y轴变化
-                                if (cType==2){//宽度变化
-                                    option['width']=!direction?cX+posix.w:posix.w-cX;
-                                    if(direction){
-                                        option['left']=posix.left+cX;
+                            'move': true, 'call_up': function () {
+                                clObj.css('cursor', 'crosshair');
+                            }, 'call_down': function (e) {
+                                var option = {};
+                                var cX = e.pageX - posix.x;//x轴变化
+                                var cY = e.pageY - posix.y;//y轴变化
+                                if (cType == 2) {//宽度变化
+                                    option['width'] = !direction ? cX + posix.w : posix.w - cX;
+                                    if (direction) {
+                                        option['left'] = posix.left + cX;
                                     }
                                 }
-                                if (cType==1){//高度变化
-                                    option['height']=!direction?cY+posix.h:posix.h-cY;
-                                    if(direction){
-                                        option['top']=posix.top+cY;
+                                if (cType == 1) {//高度变化
+                                    option['height'] = !direction ? cY + posix.h : posix.h - cY;
+                                    if (direction) {
+                                        option['top'] = posix.top + cY;
                                     }
                                 }
-                                if (cType==3){//宽高变化
-                                    if(direction=='rb'){
-                                        option['height']=posix.h+cY;
-                                        option['width']=cX+posix.w;
+                                if (cType == 3) {//宽高变化
+                                    if (direction == 'rb') {
+                                        option['height'] = posix.h + cY;
+                                        option['width'] = cX + posix.w;
                                     }
-                                    if(direction=='rt'){
-                                        option['height']=posix.h-cY;
-                                        option['width']=cX+posix.w;
-                                        option['top']=posix.top+cY;
+                                    if (direction == 'rt') {
+                                        option['height'] = posix.h - cY;
+                                        option['width'] = cX + posix.w;
+                                        option['top'] = posix.top + cY;
                                     }
-                                    if(direction=='lt'){
-                                        option['height']=posix.h-cY;
-                                        option['width']=posix.w-cX;
-                                        option['top']=posix.top+cY;
-                                        option['left']=posix.left+cX;
+                                    if (direction == 'lt') {
+                                        option['height'] = posix.h - cY;
+                                        option['width'] = posix.w - cX;
+                                        option['top'] = posix.top + cY;
+                                        option['left'] = posix.left + cX;
                                     }
-                                    if(direction=='lb'){
-                                        option['height']=posix.h+cY;
-                                        option['width']=posix.w-cX;
-                                        //option['top']=posix.top+cY;
-                                        option['left']=posix.left+cX;
+                                    if (direction == 'lb') {
+                                        option['height'] = posix.h + cY;
+                                        option['width'] = posix.w - cX;
+                                        option['left'] = posix.left + cX;
                                     }
-                                    //option['height']=!direction?cY+posix.h:posix.h-cY;
-                                    //option['width']=direction?cX+posix.w:posix.w-cX;
-                                    //if(direction){
-                                    //    option['top']=posix.top+cY;
-                                    //}
+
                                 }
                                 item.css(option);
                             }
@@ -170,8 +166,9 @@
                         return false;
                     });
 
-                    var b_dw, b_dh, b_top, b_left;
+
                     //旋转控制开始
+                    var b_dw, b_dh, b_top, b_left;
                     var rotateCl = $(settings.rotate, item);
                     rotateCl.mousedown(function (e9) {
                         event.stopPropagation();
@@ -185,7 +182,7 @@
                         var y0 = b_top + b_dh / 2 + offset.top;
                         var a1 = Math.atan2(x0 - x9, y0 - y9);
                         a1 = a1 * 180 / Math.PI;
-                        var rotate =parseInt(item.attr('data-rotate'));
+                        var rotate = parseInt(item.attr('data-rotate'));
                         $.extend(moveObj, {
                             'move': true, 'call_down': function (ev9) {
                                 var _x9 = ev9.pageX;
@@ -193,7 +190,7 @@
                                 var a2 = Math.atan2(x0 - _x9, y0 - _y9);
                                 a2 = a2 * 180 / Math.PI;
                                 var value = rotate - a2 + a1;
-                                item.attr('data-rotate', value)
+                                item.attr('data-rotate', value);
                                 item.css({
                                     "transform": "rotate(" + value + "deg)",
                                     "-webkit-transform": "rotate(" + value + "deg)",
@@ -204,7 +201,7 @@
                             }
                         });
                     });
-//旋转控制结束
+                //旋转控制结束
                 }
                 return false;
             });
